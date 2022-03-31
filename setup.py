4a4f99a0266591b9c171ba92e2b84999858706c8
Yousef <namiyousef@hotmail.com>
2022-03-31 03:34:06 +0200

    adds core package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlutils',
+    version='0.0.1',
+    description='Useful functions for Machine Learning',
+    author='Yousef Nami',
+    author_email='namiyousef@hotmail.com',
+    url='https://github.com/namiyousef/ml-utils',
+    install_requires=['torch'],
+    #package_data={}
+    packages=find_packages(exclude=('tests*', 'experiments*')),
+    license='MIT',
+    #entry_points=(),
+)
