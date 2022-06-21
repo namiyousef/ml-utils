f1812f83aa090814f3b4e2f332387ad1c350a4e5
Yousef <namiyousef@hotmail.com>
2022-06-21 22:56:22 +0100

    adds basic BaseTrainer

--- mlutils/torchtools/run.py
+++ mlutils/torchtools/run.py
@@ -4,15 +4,96 @@ import torch
 import gc
 import time
 
+# TODO need to figure out how to do the logging properly
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter
+date_format = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s', datefmt=date_format)
+# add formatter to ch
+ch.setFormatter(formatter)
+# add ch to logger
+logger.addHandler(ch)
+
+
 def call_model():
     pass
 
-class Trainer():
+
+class BaseTrainer:
+    def __init__(self, model, optimizer, loss=None, scheduler=None, metrics=None):
+        # TODO think about how metrics would work for the case of multitask learning
+
+        self.model = model
+        self.optimizer = optimizer
+        self.loss = loss
+        self.scheduler = scheduler
+        self.metrics = metrics
+
+        # TODO do you want the loss packages with or without the model? With huggingface it comes with, e.g. in the return, with normal nn tpyically you treat it
+        # as a separate parameter?
+        # todo how to package model and loss together?
+
+
+    def train(self, train_loader, epochs, val_loader=None, verbose=0):
+        # design choice: we don't specify any data related parameters here. The choice is left to the user to define them according to their dataset
+
+        for epoch_id in range(epochs):
+            self.model.train()
+            for batch_id, batch in enumerate(train_loader):
+
+                batch = self.prepare_batch(batch)
+
+                # TODO send batch_metadata to GPU, also think about how to choose which items to send and which not to
+                # can probably change this to preprocess gradient, then can have process gradient!
+                self.optimizer.zero_grad()
+
+
+                model_output_dict = self.compute_loss(batch)
+
+
+                if self.metrics:
+                    self.compute_metrics(model_output_dict)
+
+
+                # TODO metric calculation and storage in metadata
+                # TODO metric calculation for gradient??
+
+                self.process_gradient() # what if process gradient requires inputs, targets, metadata, even the loss value? e.g. things that aren't stored in the model, how we will pass that in?
+
+                model_output_dict['loss'].backward()
+                self.optimizer.step()
+                if self.scheduler:
+                    self.scheduler.step()
+
+                del model_output_dict # metadata, loss (but need to figure out: a) if deleting loss deletes computational graph, b) if metadata needs to be deleted at all?). If a), then need to change design
+                gc.collect()
+                torch.cuda.empty_cache()
+
+            if val_loader:
+                self.test(val_loader)
+
+
+    def test(self, test_loader):
+        pass
+
+
+    def save(self):
+        pass
+
     def _log(self):
         # default logging behaviour
         pass
-    def _process_gradient(self):
+    def process_gradient(self):
+        return None
         # option for clipping
+        #torch.nn.utils.clip_grad_norm_(
+        #   parameters=self.model.parameters(), max_norm=MAX_NORM
+        #) # one might want to provide this as an input parameter
         pass
     def _call_model(self):
         pass
@@ -22,6 +103,53 @@ class Trainer():
     def _save(self):
         pass # default save behavioru
     pass
+
+    def prepare_batch(self, batch):
+        batch['inputs'] = move_to_device(batch['inputs'])
+        batch['targets'] = move_to_device(batch['targets'])
+
+        # what is the purpose of metadata?
+        #batch['metadata'] = move_to_device()
+        # these are things that you explicitly don't want on the GPU. Everything else should go within inputs/targets
+        # the question now becomes, will you even need metadata?
+
+        return batch
+
+    def compute_metrics(self):
+        pass
+
+    def compute_loss(self, model_output_dict):
+        # TODO advantage of having this as a separate function is that you can automatically time using a timer wrapper
+        if self.loss:
+            # e.g. standard models
+            outputs = self.model(model_output_dict['inputs'])
+            loss = self.loss(outputs, model_output_dict['targets'])
+        else:
+            # TODO what about huggingface + multitask?
+            # e.g. HuggingFace models
+            loss, outputs = self.model(
+                labels=model_output_dict['targets'],
+                input_ids=model_output_dict['inputs']['input_ids'],
+                attention_mask=model_output_dict['inputs']['attention_mask'],
+                return_dict=False
+            )
+
+        model_output_dict['outputs'] = outputs
+
+        return model_output_dict
+
+
+
+def HuggingFaceTrainer(BaseTrainer):
+    def __init__():
+        super().__init__()
+        pass
+
+def MultiTaskTrainer(BaseTrainer):
+    def __init__():
+        super().__init__()
+        pass
+
 
 # TODO metric collection specify if at batch or epoch! Also metric should technically be able to use anything from the model
 # TODO move_to_device needs to be made much smarter and more automatic
