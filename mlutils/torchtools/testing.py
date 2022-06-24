e8cca5fea6d40f92504c1799eb8e95353c2c13a3
Yousef <namiyousef@hotmail.com>
2022-06-24 11:23:27 +0100

    fixes bug on assert_tensor_objects_equal

--- mlutils/torchtools/testing.py
+++ mlutils/torchtools/testing.py
@@ -3,8 +3,10 @@ import torch
 
 def assert_tensor_objects_equal(obtained_object, expected_object, check_keys=False, sort_objects=True):
     assert type(obtained_object) == type(expected_object), TypeError(f'object has type={type(obtained_object)} but expected_object has {type(expected_object)}')
-    assert len(obtained_object) == len(expected_object), ValueError(f'objects have different lengths: obtained={len(obtained_object)}, expected={len(expected_object)}')
     if isinstance(obtained_object, dict):
+        assert len(obtained_object) == len(expected_object), ValueError(
+            f'objects have different lengths: obtained={len(obtained_object)}, expected={len(expected_object)}')
+
         if sort_objects:
             obtained_object = dict(sorted(obtained_object.items()))
             expected_object = dict(sorted(expected_object.items()))
