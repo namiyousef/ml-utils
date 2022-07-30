d533bd4271a66857a738e4922c14137e040ff783
Yousef <namiyousef@hotmail.com>
2022-07-30 11:59:51 +0200

    adds test for probabilisitc loader without shuffling

--- tests/torchtools/test_data.py
+++ tests/torchtools/test_data.py
@@ -3,7 +3,7 @@ import torch
 import random
 
 
-from mlutils.torchtools.data import get_curriculum_dataloader
+from mlutils.torchtools.data import get_curriculum_dataloader, get_probabilistic_curriculum_dataloader
 
 from tests.torchtools.utils import SimpleDataset
 
@@ -28,6 +28,7 @@ class TestData(unittest.TestCase):
         self.batch_size = 2
         self.X = torch.ones((self.length, 2))
         self.y = torch.zeros(self.length)
+        self.epochs = 2
     def test_curriculum_loader(self):
         dataset = SimpleDataset(self.X, self.y)
         logger.info('Test 1 - test curriculum basic')
@@ -60,5 +61,68 @@ class TestData(unittest.TestCase):
 
         for i, batch in enumerate(loader):
             assert (batch['instance_ids'] == torch.tensor(difficulty_indices_shuffled[i*self.batch_size: (i+1)*self.batch_size])).all()
+
+    def test_probabilisitic_curriculum_loader(self):
+
+        dataset = SimpleDataset(self.X, self.y)
+        difficulty_indices = [
+            [9, 8, 7], [6, 5], [4, 3, 2], [1, 0]
+        ]
+        logger.info('Test 1 - test curriculum no shuffling')
+        epochs = 6
+
+        loader = get_probabilistic_curriculum_dataloader(
+            dataset,
+            difficulty_indices,
+            batch_size=self.batch_size,
+            drop_last=False,
+            num_phases_after_curriculum=epochs - len(difficulty_indices),
+            intra_shard_shuffle=False,
+            inter_shard_shuffle=False
+        )
+        expected_visible_data = []
+        for epoch in range(epochs):
+            if epoch < len(difficulty_indices):
+                expected_visible_data += difficulty_indices[epoch]
+            for i, batch in enumerate(loader):
+                instance_ids = batch['instance_ids']
+                expected_instance_ids = torch.tensor([
+                    expected_visible_data[j] for j in range(i*self.batch_size, min((i+1)*self.batch_size, len(expected_visible_data)))
+                ])
+                assert (instance_ids == expected_instance_ids).all()
+
+        logger.info('Test 2 - test with intra batch shuffling')
+        seed = 0
+        loader = get_probabilistic_curriculum_dataloader(
+            dataset,
+            difficulty_indices,
+            batch_size=self.batch_size,
+            drop_last=False,
+            num_phases_after_curriculum=epochs - len(difficulty_indices),
+            intra_shard_shuffle=seed,
+            inter_shard_shuffle=False
+        )
+
+        shards = []
+        for epoch in range(epochs):
+            if epoch < len(difficulty_indices):
+                shards.append(difficulty_indices[epoch])
+                for i in range(len(shards))
+                random.Random(seed).shuffle(difficulty_indices_shuffled[i])  # need to run twice...
+
+                expected_visible_data += difficulty_indices[epoch]
+            else:
+            for i, batch in enumerate(loader):
+                instance_ids = batch['instance_ids']
+                expected_instance_ids = torch.tensor([
+                    expected_visible_data[j] for j in
+                    range(i * self.batch_size, min((i + 1) * self.batch_size, len(expected_visible_data)))
+                ])
+                assert (instance_ids == expected_instance_ids).all()
+
+        logger.info('Test 3 - test with inter batch shuffling')
+
+
+        logger.info('Test 4 - test all shuffling')
 
 
