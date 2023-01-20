b9adfa78a7ed560bfe50bbd23331d49fdad9e684
Yousef <namiyousef@hotmail.com>
2023-01-20 19:00:42 +0000

    adds requests package to setup

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setup(
     author='Yousef Nami',
     author_email='namiyousef@hotmail.com',
     url='https://github.com/namiyousef/ml-utils',
-    install_requires=['torch', 'sklearn', 'colab-dev-tools'],
+    install_requires=[
+        'torch', 'sklearn', 'colab-dev-tools', 'requests'
+    ],
     #package_data={}
     packages=find_packages(exclude=('tests*', 'experiments*')),
     license='MIT',
